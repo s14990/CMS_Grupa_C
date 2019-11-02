de7f8b9de5c36603d06cbb619ac06086c2742dc7
s14990 <s14990@pjwtk.edu.pl>
2019-11-02 20:59:28 +0100

    init_commit

---
+++ CMS_Grupa_C/Controllers/MediaFilesController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CMS_Grupa_C.Models;
+
+namespace CMS_Grupa_C.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MediaFilesController : ControllerBase
+    {
+        private readonly cmsmainContext _context;
+
+        public MediaFilesController(cmsmainContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/MediaFiles
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MediaFile>>> GetMediaFile()
+        {
+            return await _context.MediaFile.ToListAsync();
+        }
+
+        // GET: api/MediaFiles/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<MediaFile>> GetMediaFile(int id)
+        {
+            var mediaFile = await _context.MediaFile.FindAsync(id);
+
+            if (mediaFile == null)
+            {
+                return NotFound();
+            }
+
+            return mediaFile;
+        }
+
+        // PUT: api/MediaFiles/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMediaFile(int id, MediaFile mediaFile)
+        {
+            if (id != mediaFile.FileId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(mediaFile).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MediaFileExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/MediaFiles
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<MediaFile>> PostMediaFile(MediaFile mediaFile)
+        {
+            _context.MediaFile.Add(mediaFile);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMediaFile", new { id = mediaFile.FileId }, mediaFile);
+        }
+
+        // DELETE: api/MediaFiles/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<MediaFile>> DeleteMediaFile(int id)
+        {
+            var mediaFile = await _context.MediaFile.FindAsync(id);
+            if (mediaFile == null)
+            {
+                return NotFound();
+            }
+
+            _context.MediaFile.Remove(mediaFile);
+            await _context.SaveChangesAsync();
+
+            return mediaFile;
+        }
+
+        private bool MediaFileExists(int id)
+        {
+            return _context.MediaFile.Any(e => e.FileId == id);
+        }
+    }
+}
---
+++ CMS_Grupa_C/Startup.cs
@@ -0,0 +1,79 @@
+using CMS_Grupa_C.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+
+namespace CMS_Grupa_C
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            // In production, the React files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/build";
+            });
+
+            services.AddControllers().AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            });
+
+            var connection = "Server=tcp:cms-grupa-3.database.windows.net,1433;Initial Catalog=cms-main;User ID=cms_admin;Password=useruser0!;MultipleActiveResultSets =False;Encrypt=True;TrustServerCertificate=False;";
+            services.AddDbContext<cmsmainContext>(options => options.UseSqlServer(connection));
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
+            });
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseReactDevelopmentServer(npmScript: "start");
+                }
+            });
+        }
+    }
+}
